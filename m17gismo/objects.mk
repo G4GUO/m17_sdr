2b577c4e89a1ce0b268a8259973eefa001922ebf
Charles G4GUO <g4guo@arrl.net>
2023-11-17 22:26:16 +0000

    Updating Makefile

--- m17gismo/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lLimeSuite -liio -lncurses -lpthread -lcodec2 -lpulse-simple
-
